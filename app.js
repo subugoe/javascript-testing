b8c53ec9beec2b1b68c4743750ac6182fae978c1
Dennis Neumann <neumann@sub.uni-goettingen.de>
2018-07-02 11:41:13 +0200

    Add open/close functionality

--- app.js
+++ app.js
@@ -1,11 +1,27 @@
 'use strict';
 
-var DOMa = {
-	addClass: function(css, newClass) {
-		
-	}
+var domAccess = {
+		applyStates: function (newStates) {
+			for (var i = 0; i < newStates.length; i++) {
+				var current = newStates[i];
+				this[current.method](current.selector, current.value);
+			}
+		},
+		removeClass: function(selector, value) {
+			$(selector).removeClass(value);
+		},
+		addClass: function (selector, value) {
+			$(selector).addClass(value);
+		}
 };
 
 $(document).ready(function () {
-	alert(DOMa.variable);
+		$("#openButton").click(function () {
+			var newStates = business.openMainArea();
+			domAccess.applyStates(newStates);
+		});
+		$("#closeButton").click(function () {
+			var newStates = business.closeMainArea();
+			domAccess.applyStates(newStates);
+		});
 });
---
+++ business-logic.js
@@ -0,0 +1,20 @@
+'use strict';
+
+var business = {
+	openMainArea: function () {
+		var newStates = [
+			{ selector: "#closeButton", method: "removeClass", value: "hidden" },
+			{ selector: "#openButton", method: "addClass", value: "hidden" },
+			{ selector: "#mainArea", method: "removeClass", value: "hidden" }	
+		];		
+		return newStates;
+	},
+	closeMainArea: function () {
+		var newStates = [
+			{ selector: "#closeButton", method: "addClass", value: "hidden" },
+			{ selector: "#openButton", method: "removeClass", value: "hidden" },
+			{ selector: "#mainArea", method: "addClass", value: "hidden" }			
+		];
+		return newStates;		
+	}
+};
