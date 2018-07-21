eaa62fdab4aaf53bf7f6897f2d5a2d5adaf2554e
Dennis Neumann <neumann@sub.uni-goettingen.de>
2018-07-21 18:41:15 +0200

    Format code

--- app/scripts/event-handlers.js
+++ app/scripts/event-handlers.js
@@ -1,27 +1,27 @@
 'use strict';
 
 document.addEventListener('DOMContentLoaded', function() {
-		function id(elemId) {
-			return document.getElementById(elemId);
-		}
-		function selector(elemSelector) {
-			return document.querySelector(elemSelector);
-		}
-		
-		id("openButton").onclick = function () {
-			var newStates = business.openMainArea();
-			domChanger.applyStates(newStates);
-		};
-		id("closeButton").onclick = function() {
-			var newStates = business.closeMainArea();
-			domChanger.applyStates(newStates);
-		};
-		id("computeButton").onclick = function () {
-			var number1 = id("firstNumber").value;
-			var number2 = id("secondNumber").value;
-			var operator = selector("input[name='operand']:checked").value;;
+	function id(elemId) {
+		return document.getElementById(elemId);
+	}
+	function selector(elemSelector) {
+		return document.querySelector(elemSelector);
+	}
+	
+	id("openButton").onclick = function () {
+		var newStates = business.openMainArea();
+		domChanger.applyStates(newStates);
+	};
+	id("closeButton").onclick = function() {
+		var newStates = business.closeMainArea();
+		domChanger.applyStates(newStates);
+	};
+	id("computeButton").onclick = function () {
+		var number1 = id("firstNumber").value;
+		var number2 = id("secondNumber").value;
+		var operator = selector("input[name='operand']:checked").value;;
 
-			var newStates = business.compute(number1, number2, operator);
-			domChanger.applyStates(newStates);
-		};
+		var newStates = business.compute(number1, number2, operator);
+		domChanger.applyStates(newStates);
+	};
 });
